cc1628dc87ac492b6b84de14374eaea89f298740
shadab <shad.sayeed2@gmail.como>
2017-07-08 23:58:36 +0530

    codehaus dependency added

---
+++ src/main/java/PassArgument/ConvertDollarsToRupee/App.java
@@ -0,0 +1,13 @@
+package PassArgument.ConvertDollarsToRupee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
